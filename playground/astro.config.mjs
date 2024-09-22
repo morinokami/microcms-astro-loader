36ab760fe9a13c41e1b0a2f2f6b77f4eb9faf476
Shinya Fujino <shf0811@gmail.com>
2024-09-23 01:37:10 +0900

    Add Vercel adapter

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://microcms-astro-loader-demo.vercel.app",
+  output: 'server',
+  adapter: vercel(),
   env: {
     schema: {
       MICROCMS_API_KEY: envField.string({
